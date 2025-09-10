4dd1f4b48d1ab26a140b7dac4c0a302352936be8
diamondpete <87245367+diamondpete@users.noreply.github.com>
2025-09-09 20:21:53 -0400

    feat(pervcity): Cover PervCity Site Changes

--- Contents/Code/networkPervCity.py
+++ Contents/Code/networkPervCity.py
@@ -3,6 +3,7 @@ import PAutils
 
 
 def search(results, lang, siteNum, searchData):
+    cookies = {'warning_cookie': '1'}
     searchResults = []
 
     googleResults = PAutils.getFromGoogleSearch(searchData.title, siteNum)
@@ -12,11 +13,14 @@ def search(results, lang, siteNum, searchData):
             searchResults.append(sceneURL)
 
     for sceneURL in searchResults:
-        req = PAutils.HTTPRequest(sceneURL)
+        req = PAutils.HTTPRequest(sceneURL, cookies=cookies)
         detailsPageElements = HTML.ElementFromString(req.text)
 
         titleNoFormatting = detailsPageElements.xpath('//h1')[0].text_content().strip()
-        subSite = detailsPageElements.xpath('//div[@class="about"]//h3')[0].text_content().replace('About', '').strip()
+        try:
+            subSite = detailsPageElements.xpath('//div[@class="about"]//h3')[0].text_content().replace('About', '').strip()
+        except:
+            subSite = ''
         curID = PAutils.Encode(sceneURL)
 
         date = ''
@@ -31,48 +35,73 @@ def search(results, lang, siteNum, searchData):
         else:
             score = 100 - Util.LevenshteinDistance(searchData.title.lower(), titleNoFormatting.lower())
 
-        results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [PervCity/%s]' % (titleNoFormatting, subSite), score=score, lang=lang))
+        results.Append(MetadataSearchResult(id='%s|%d' % (curID, siteNum), name='%s [PervCity/%s]' % (titleNoFormatting, subSite) if subSite else '%s [PervCity]' % titleNoFormatting, score=score, lang=lang))
 
     return results
 
 
 def update(metadata, lang, siteNum, movieGenres, movieActors, movieCollections, art):
+    cookies = {'warning_cookie': '1'}
     metadata_id = str(metadata.id).split('|')
     sceneURL = PAutils.Decode(metadata_id[0])
 
-    req = PAutils.HTTPRequest(sceneURL)
+    req = PAutils.HTTPRequest(sceneURL, cookies=cookies)
     detailsPageElements = HTML.ElementFromString(req.text)
 
     # Title
-    metadata.title = detailsPageElements.xpath('//h1')[0].text_content().strip()
+    metadata.title = PAutils.parseTitle(detailsPageElements.xpath('//h1')[0].text_content().strip(), siteNum)
 
     # Summary
-    metadata.summary = detailsPageElements.xpath('//div[@class="infoBox clear"]/p')[0].text_content().strip()
+    try:
+        metadata.summary = detailsPageElements.xpath('//div[@class="infoBox clear"]/p')[0].text_content().strip()
+    except:
+        metadata.summary = detailsPageElements.xpath('//h3[@class="description"]')[0].text_content().strip()
 
     # Studio
     metadata.studio = 'PervCity'
 
     # Tagline and Collection(s)
-    metadata.tagline = detailsPageElements.xpath('//div[@class="about"]//h3')[0].text_content().replace('About', '').strip()
-    movieCollections.addCollection(metadata.tagline)
+    tagline = detailsPageElements.xpath('//div[@class="about"]//h3')
+    if tagline:
+        metadata.tagline = tagline[0].text_content().replace('About', '').strip()
+        movieCollections.addCollection(metadata.tagline)
+    elif PAsearchSites.getSearchSiteName(siteNum).replace(' ', '') != metadata.studio:
+        metadata.tagline = PAsearchSites.getSearchSiteName(siteNum)
+        movieCollections.addCollection(metadata.tagline)
+    else:
+        movieCollections.addCollection(metadata.studio)
 
     # Genres
+    for genreLink in detailsPageElements.xpath('//div[@class="tagcats"]/a'):
+        genreName = genreLink.text_content().strip()
+
+        movieGenres.addGenre(genreName)
 
     # Actor(s)
     date = ''
-    actors = detailsPageElements.xpath('//h3/span/a')
+    actors = detailsPageElements.xpath('//h2/span/a | //h3/span/a')
     for actorLink in actors:
         actorName = actorLink.text_content().strip()
         actorPhotoURL = ''
 
-        modelURL = actorLink.xpath('.//@href')[0].replace(PAsearchSites.getSearchBaseURL(siteNum).replace('www.', ''), PAsearchSites.getSearchBaseURL(1165).replace('www.', ''))
-        req = PAutils.HTTPRequest(modelURL)
-        actorsPageElements = HTML.ElementFromString(req.text)
-        actorPhotoURL = actorsPageElements.xpath('//div[@class="starPic"]/img/@src')[0]
+        try:
+            modelURL = actorLink.xpath('.//@href')[0].replace(PAsearchSites.getSearchBaseURL(siteNum).replace('www.', ''), PAsearchSites.getSearchBaseURL(1165).replace('www.', ''))
+            req = PAutils.HTTPRequest(modelURL, cookies=cookies)
+            actorsPageElements = HTML.ElementFromString(req.text)
+            actorPhotoURL = actorsPageElements.xpath('//div[@class="starPic" or @class="bioBPic"]/img/@src')[0]
+        except:
+            modelURL = actorLink.xpath('.//@href')[0].replace('www.', '')
+            req = PAutils.HTTPRequest(modelURL, cookies=cookies)
+            actorsPageElements = HTML.ElementFromString(req.text)
+            actorPhotoURL = actorsPageElements.xpath('//div[@class="starPic" or @class="bioBPic"]/img/@src')[0]
+
 
         if not date:
-            for scene in actorsPageElements.xpath('//div[@class="videoBlock"]'):
-                if scene.xpath('.//h3')[0].text_content().replace('...', '').strip().lower() in metadata.title.lower():
+            for scene in actorsPageElements.xpath('//div[@class="videoBlock" or @class="videoContent"]'):
+                cleanTitle = re.sub(r'\W', '', metadata.title).lower()
+                h2CleanTitle = re.sub(r'\W', '', scene.xpath('.//h3')[0].text_content().replace('...', '').strip()).lower()
+                h3CleanTitle = re.sub(r'\W', '', scene.xpath('.//h2/a')[0].text_content().replace('...', '').strip()).lower()
+                if h2CleanTitle in cleanTitle or h3CleanTitle in cleanTitle:
                     date = actorsPageElements.xpath('.//div[@class="date"]')[0].text_content()
 
             if date:
@@ -95,8 +124,12 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, movieCollections, 
             art.append(img)
 
     Log('Artwork found: %d' % len(art))
+    postersClean = list()
     for idx, posterUrl in enumerate(art, 1):
-        if not PAsearchSites.posterAlreadyExists(posterUrl, metadata):
+        # Remove Timestamp and Token from URL
+        cleanUrl = posterUrl.split('?')[0]
+        postersClean.append(cleanUrl)
+        if not PAsearchSites.posterAlreadyExists(cleanUrl, metadata):
             # Download image file for analysis
             try:
                 image = PAutils.HTTPRequest(posterUrl)
@@ -106,11 +139,13 @@ def update(metadata, lang, siteNum, movieGenres, movieActors, movieCollections, 
                 # Add the image proxy items to the collection
                 if width > 1:
                     # Item is a poster
-                    metadata.posters[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+                    metadata.posters[cleanUrl] = Proxy.Media(image.content, sort_order=idx)
                 if width > 100:
                     # Item is an art item
-                    metadata.art[posterUrl] = Proxy.Media(image.content, sort_order=idx)
+                    metadata.art[cleanUrl] = Proxy.Media(image.content, sort_order=idx)
             except:
                 pass
+
+    art.extend(postersClean)
 
     return metadata
